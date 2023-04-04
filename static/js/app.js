d2339e8fe8c5946d1a684d862f3fba34e2f04efc
Cold Magic <>
2023-04-04 07:48:03 +0000

    Made changes to colour. Also fixed functionality. This is now a working version.

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,7 @@ const subjectButtons = document.querySelectorAll('.sidebar button');
 const chatWindow = document.querySelector('.chat-window');
 const form = document.querySelector('form');
 const input = document.querySelector('input');
+let askKateMode = false;
 
 subjectButtons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -15,27 +16,22 @@ subjectButtons.forEach((button) => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const message = input.value.trim();
-  if (message.length === 0) return;
-  addMessage('user', message);
-  input.value = '';
-  fetch('/api/chat', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ subject: getSelectedSubject(), message }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      addMessage('ai', data.response);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+  sendMessage();
 });
 
 function addMessage(role, message) {
   const messageElement = document.createElement('div');
-  messageElement.classList.add('message', role);
+  let className = '';
+
+  if (role === 'user') {
+    className = 'user';
+  } else if (role === 'ai') {
+    className = 'ai';
+  } else {
+    className = 'system-message';
+  }
+
+  messageElement.classList.add('message', className);
   messageElement.textContent = message;
   chatWindow.appendChild(messageElement);
   chatWindow.scrollTop = chatWindow.scrollHeight;
@@ -47,7 +43,14 @@ function getSelectedSubject() {
 }
 
 function askKATE() {
-  const message = 'Ask KATE Anything';
+  askKateMode = true;
+  addMessage('system', 'Ask KATE Anything: KATE is ready for your question.');
+}
+
+function sendMessage() {
+  const message = input.value.trim();
+  if (message.length === 0) return;
+
   addMessage('user', message);
   fetch('/api/chat', {
     method: 'POST',
@@ -61,4 +64,6 @@ function askKATE() {
     .catch((error) => {
       console.error('Error:', error);
     });
+
+  input.value = '';
 }
