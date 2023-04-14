50e225d69a1d63cac534da4f6b266e402d33fdff
Cold Magic <>
2023-04-14 04:55:03 +0000

    updated some other changes. Now Using an earlier version.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def chat():
     subject = data.get('subject')
     message = data.get('message')
 
-    ai_response = ask_kate(human_input=message)
+    ai_response = ask_kate(message)
 
     return jsonify({'response': ai_response})
 
